d636d9189940d98b3f5abaa6410924bb8d8da0a5
Kevin <stradtkt22@gmail.com>
2020-09-02 23:33:18 -0400

    Added SVM project for iris

--- SuportVectorMachines/svm.py
+++ SuportVectorMachines/svm.py
@@ -16,7 +16,7 @@ print(cancer['DESCR'])
 df_feat = pd.DataFrame(cancer['data'], columns=cancer['feature_names'])
 df_feat.head(2)
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 X = df_feat
 y = cancer['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
@@ -29,7 +29,6 @@ from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(y_test, predictions))
 print(classification_report(y_test, predictions))
 
-from sklearn.grid_search import GridSearchCV
 param_grid = {'C':[0.1, 1, 10, 100, 1000], 'gamma':[1, 0.1, 0.01, 0.001, 0.0001]}
 grid = GridSearchCV(SVC(), param_grid, verbose=3)
 grid.fit(X_train, y_train)
